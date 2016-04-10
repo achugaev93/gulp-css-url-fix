a6230f090bb744627551b05d2ba8ea5fef48662b
Alex Chugaev <achugaev93@gmail.com>
2016-04-10 16:10:31 +0300

    Avoid processing data-uris.

--- index.js
+++ index.js
@@ -10,21 +10,37 @@ var isAbsolute = function(p) {
 };
 
 var rebaseUrls = function(css, options) {
-    return rework(css)
-        .use(rework.url(function(url){
-            if (isAbsolute(url) && validator.isURL(url)) {
-                return url;
-            }
-            var absolutePath = path.join(options.currentDir, url)
-            var p = path.relative(options.root, absolutePath);
+    return rework(css).use(rework.url(function (url) {
+        
+        // Skip absolute addresses
+        
+        if (isAbsolute(url) && validator.isURL(url)) {
+            return url;
+        }
+        
+        // Do not modify data-uri
+        
+        if (url.indexOf('data:') === 0) {
+            return url;
+        }
+        
+        // Make absolute path
+        
+        var absolutePath = path.join(options.currentDir, url)
+        
+        // Get path relative to the root dir
+        
+        var p = path.relative(options.root, absolutePath);
 
-            if (process.platform === 'win32') {
-                p = p.replace(/\\/g, '/');
-            }
+        // Fix delimiters on Windows
 
-            return p;
-        }))
-        .toString();
+        if (process.platform === 'win32') {
+            p = p.replace(/\\/g, '/');
+        }
+
+        return p;
+    }))
+    .toString();
 };
 
 module.exports = function(options) {
@@ -32,11 +48,16 @@ module.exports = function(options) {
     var root = options.root || '.';
 
     return through.obj(function(file, enc, cb) {
+        
+        // Fix paths in current CSS file
+        
         var css = rebaseUrls(file.contents.toString(), {
             currentDir: path.dirname(file.path),
             root: path.join(file.cwd, root)
         });
 
+        // Replace original file contents with fixed one
+        
         file.contents = new Buffer(css);
 
         this.push(file);
